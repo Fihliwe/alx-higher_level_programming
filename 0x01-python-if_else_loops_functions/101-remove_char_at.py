3a3eb5a972aa87200ab12f7d9b1536b9d05955fa
Fihliwe <siyabulelmncube@gmail.com>
2023-10-03 06:57:13 -0700

    102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-def remove_char_at(str, n):
 
-    if n < 0:
-        return str
+def remove_char_at(input_str, n):
+    if n < 0 or n >= len(input_str):
+        return input_str
 
-    return (str[:n] + str[n + 1:]
+    result = ""
+    
+    for i in range(len(input_str)):
+        if i != n:
+            result += input_str[i]
+    
+    return result

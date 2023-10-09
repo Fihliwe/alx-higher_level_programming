88514fd7d5b0df8869f00e00e79e64d904b1fada
Fihliwe <siyabulelmncube@gmail.com>
2023-10-08 23:39:40 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+
+    for i in range(len(my_string)):
+
+        if i in my_string del ['c' ,'C']:
+
+            new_list = my_list[i]
+
+            return (new_list)

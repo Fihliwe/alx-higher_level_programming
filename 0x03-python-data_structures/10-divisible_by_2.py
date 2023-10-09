a51be7fc4e804520256ee62071124a3659225d11
Fihliwe <siyabulelmncube@gmail.com>
2023-10-09 02:34:46 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    multi_2 = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multi_2.append(True)
+
+        else:
+            multi_2.append(False)
+
+    return (multi_2)
+

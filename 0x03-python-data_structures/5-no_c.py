e756ff3cab73c9bf5f40f601d1e6d681ce71a38e
Fihliwe <siyabulelmncube@gmail.com>
2023-10-09 01:09:55 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 
 def no_c(my_string):
-
-    new_string = ""
-
-    for char in my_string:
-        if char not in ['c', 'C']:
-            new_string += char
-
-    return (new_string)
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))

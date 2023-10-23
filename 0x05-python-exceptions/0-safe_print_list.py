d2e5272a17d08602d44a513310ed1ed1e67e9af2
Fihliwe <siyabulelmncube@gmail.com>
2023-10-23 04:38:36 -0700

    advanced

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-
-    increment = 0
+    num = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i], end=""))
-            increment += 1
+            print(my_list[i], end="")
+            num += 1
         except IndexError:
             break
     print("")
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
- def safe_print_division(a, b):
-     try:
-         division = a / b
-     
-     except (ZeroDivisionError):
-         division = None
-     
-     finally:
-         print("Inside result: {}".format(division))
-         return (division)
-
-
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+        return div

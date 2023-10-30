22187a4dfdc8463a19d09b890dbe032b758b3485
Fihliwe <siyabulelmncube@gmail.com>
2023-10-30 03:56:39 -0700

    tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Function that adds two integer
+Usage:
+    add_integer(number_one, number_two)
+'''
+
+
+def add_integer(a, b=98):
+    '''
+    Function that adds two integer
+    Return: result of two number'''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    add = int(a) + int(b)
+
+    return add

648c6bb6044836d57d9f41704c5692bfc375a3ca
Fihliwe <siyabulelmncube@gmail.com>
2023-11-06 05:16:26 -0800

    1 with test

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Module for clas MyList and function print_sorted'''
+
+class MyList(list):
+    ''' class for mylist that inherits for list'''
+    pass
+
+    def print_sorted(self):
+        '''function to sort list in ascending order'''
+        asc_list = sorted(list(self))
+        print(asc_list)
+

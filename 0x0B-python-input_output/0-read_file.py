15804e3d00018d742145f022bf70731201c09c2a
Fihliwe <siyabulelmncube@gmail.com>
2023-11-07 02:04:02 -0800

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-'''Module for read_file function'''
+"""Module for read_file function."""
 
 def read_file(filename=""):
-    '''reads file and print to stdout'''
+    """reads file and print to stdout"""
 
     with open(filename, encoding="UTF-8" as f:
             print(f.read(), end="")

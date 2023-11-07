b507c4a2025336f54d657a0d32c7436c25b6a0eb
Fihliwe <siyabulelmncube@gmail.com>
2023-11-07 04:04:18 -0800

    12 pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''module for pascal_triangle'''
+
+
+def pascal_triangle(n):
+    ''' that returns a list of lists of integers'''
+    if n <= 0:
+        return ([])
+    
+    triangle = [[1]]
+    while len(tri) != n:
+        tri = triangle[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return (triangle)

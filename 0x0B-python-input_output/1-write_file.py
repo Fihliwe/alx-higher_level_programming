944785cfed1ef1dfdc35d1bcc6051cf2f46457e0
Fihliwe <siyabulelmncube@gmail.com>
2023-11-07 02:01:57 -0800

    first 3 tasks

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Module for function write_file'''
+
+def write_file(filename="", text=""):
+    ''' writes a string to a text file'''
+
+    with open(filename, "w", encoding="UTF-8") as f:
+        return (f.write(text))
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for function append_write'''
+
+def append_write(filename="", text=""):
+    '''appends a string to the end of a file'''
+    
+    with open(filename, "a", encoding="UTF-8") as f:
+        return (f.write(text))

950b27aee1f2d864e3c0d70c4757026a36bf826e
Fihliwe <siyabulelmncube@gmail.com>
2023-11-07 02:09:38 -0800

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 '''Module for function write_file'''
+
 
 def write_file(filename="", text=""):
     ''' writes a string to a text file'''
-
     with open(filename, "w", encoding="UTF-8") as f:
         return (f.write(text))

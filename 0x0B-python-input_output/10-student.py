75ddbc8a234a859c427949038fc571db14376da6
Fihliwe <siyabulelmncube@gmail.com>
2023-11-07 03:34:26 -0800

    10 student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''module for student'''
+
+
+class Student:
+    '''defines a stdudent'''
+
+
+    def __init__(self, first_name, last_name, age):
+        '''initializing public instances'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' that retrieves a dictionary representation of a Student instance'''
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return (self.__dict__)

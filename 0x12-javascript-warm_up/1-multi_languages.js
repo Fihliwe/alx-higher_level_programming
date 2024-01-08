7fc5e5154aa87cb952c07450183e4d307436d4c1
Fihliwe <siyabulelmncube@gmail.com>
2024-01-08 05:34:08 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints 3 lines
+
+const firstLine = 'C is fun \n';
+const secondLine = 'Python is cool \n';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine, secondLine, thirdLine);

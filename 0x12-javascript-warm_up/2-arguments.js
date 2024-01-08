67470410975a057e232608852a33cb887a581870
Fihliwe <siyabulelmncube@gmail.com>
2024-01-08 06:20:17 -0800

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints a message depending on the arguemnts passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

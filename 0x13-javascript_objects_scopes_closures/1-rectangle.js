bf39e85be4c9cdeee2e3e2a056b6bd29722d9bd9
Fihliwe <siyabulelmncube@gmail.com>
2024-01-09 03:59:47 -0800

    Rectangle #1, semistandard

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 // rectangle class
 // constructor will be added with w and h and initialized
 
-module.exports = class Rectangle{
-	constructor(w, h){
-		this.width = w;
-		this.height = h;
-	}
-}
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};

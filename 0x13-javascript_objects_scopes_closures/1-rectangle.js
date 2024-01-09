fe93c6c02ba5bcb3ae747d9e210ec050000c6a87
Fihliwe <siyabulelmncube@gmail.com>
2024-01-09 03:51:04 -0800

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// rectangle class
+// constructor will be added with w and h and initialized
+
+module.exports = class Rectangle{
+	constructor(w, h){
+		this.width = w;
+		this.height = h;
+	}
+}

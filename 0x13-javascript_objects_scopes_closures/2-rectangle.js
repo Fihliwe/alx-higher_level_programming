1d8bc2cf8cc707ef3c90c2e054460d66a24a418f
Fihliwe <siyabulelmncube@gmail.com>
2024-01-09 05:58:38 -0800

    fix Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
-// rectangle class
-// constructor will be added with w and h and initialized
-
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-    return {};
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 };

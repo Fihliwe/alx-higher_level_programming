961bbe56c5d9c612137d0141d475ad19e2fad9b6
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-01-11 18:00:42 +0200

    added cursor and connection

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module that lists states from the hbtn_0e_0_usa'''
+import MySQLdb
+from sys import argv
+
+
+
+'''The code should not be executed when imported so (if main) should be used'''
+if __name__ == '__main__':
+
+    '''make a connection to the database'''
+    data_base = MySQLdb.connect(host="localhost", port=3306, mysql_username=argv[1], mysql_password=argv[2], data_base_name=argv[3])\
+    
+    '''creating a cursor object to execute SQL quaeries'''
+    cursor = data_base.cursor()
+    query = "SELECT *FROM states"
+    cursor.execute(query)
+
+    '''iterate trough to show the results'''
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+
+    '''close the cursor and database connection'''
+    cursor.close()
+    data_base.close()

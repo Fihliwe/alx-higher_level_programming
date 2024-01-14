af255c1af4ba7422f3343183fe33d95ec59bbe36
Fihliwe <siyabulelmncube@gmail.com>
2024-01-14 12:29:34 -0800

    exe

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+Module that list all `states` from the database `hbtn_0e_0_usa`
+"""
 from sys import argv
+import MySQLdb
 
 
-# The code should not be executed when imported
-if __name__ == '__main__':
+def main():
+    """
+    Connecting to database and retrieving the state names
+    """
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states")
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()

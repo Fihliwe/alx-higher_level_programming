c9aa97a7457b88330cde3c4dee1d6dea9b06ad5e
Fihliwe <siyabulelmncube@gmail.com>
2024-01-25 05:33:08 -0800

    2. cURL Method

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that sends a DELETE request to the URL passed as the first argument
-#displays the body of the response
-curl -s "$1" -X DELETE
+# displays the body of the response
+curl -s "$1" -X DELETEi

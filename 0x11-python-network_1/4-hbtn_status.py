67a62b0c58346819293bf5be9cfbdaf65ea99b8d
Fihliwe <siyabulelmncube@gmail.com>
2024-01-26 09:54:49 -0800

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """Module for  Python script that fetches https://alx-intranet.hbtn.io/status"""
+
 import requests
 
 
 if __name__ == "__main__":
-    response = request.get("https://alx-intranet.hbtn.io/status")
-    print(Body response:)
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text)
+    print("\t- content: {}".format(response.text))
